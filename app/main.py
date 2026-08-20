efebe31b8b2b1ecfd20b7d35ce85a77413a42e5c
optus-56 <bijanbakabal45@gmail.com>
2026-08-21 01:26:32 +0545

    added cors

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://spotify-search-y345.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
